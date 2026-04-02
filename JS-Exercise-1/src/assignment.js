6bf326f552d8191251e36de4d45b1f664a741623
Tavern <tavernkinder711@gmail.com>
2026-04-02 03:27:07 -0600

    Put indipendent files in their own folder for submission

--- JS-Exercise-1/src/assignment.js
+++ JS-Exercise-1/src/assignment.js
@@ -1,19 +1,19 @@
 function getWelcomeMessage(name) {
-    return "Hello, " + name + "!";
+  return "Hello, " + name + "!";
 }
-function doDiv(a , b) {b
-    return a / b;
-
+function doDiv(a, b) {
+  b;
+  return a / b;
 }
-function doMulti(a , b) {
-    return a * b;
+function doMulti(a, b) {
+  return a * b;
 }
 function main() {
-    alert("Welcome");
-    let username = prompt("What is your name:");
-    alert(getWelcomeMessage(username));
-    let num1 = parseFloat(prompt("Enter the first number:"));
-    let num2 = parseFloat(prompt("Enter the second number:"));
-    alert(num1 + " ÷ " + num2 + " = " + doDiv(num1, num2));
-    alert(num1 + " × " + num2 + " = " + doMulti(num1, num2)); 
+  alert("Welcome");
+  let username = prompt("What is your name:");
+  alert(getWelcomeMessage(username));
+  let num1 = parseFloat(prompt("Enter the first number:"));
+  let num2 = parseFloat(prompt("Enter the second number:"));
+  alert(num1 + " ÷ " + num2 + " = " + doDiv(num1, num2));
+  alert(num1 + " × " + num2 + " = " + doMulti(num1, num2));
 }
--- independent_files/movie-ticket-eligibility-3.js
+++ independent_files/movie-ticket-eligibility-3.js
@@ -2,22 +2,20 @@
 let age = 21;
 let isStudent = true;
 
-
 // Print Age and Student status
 if (age < 18) {
-    console.log("Person is a Minor")
-} else if (age >= 18){
-    console.log("Person is an Adult")
+  console.log("Person is a Minor");
+} else if (age >= 18) {
+  console.log("Person is an Adult");
 }
 
 if (isStudent) {
-    console.log("Person is a Student")
+  console.log("Person is a Student");
 }
-
 
 // Check age and student status for ticket eligibility
 if (age > 18 || isStudent) {
-    console.log("Discount ticket granted ✅")
+  console.log("Discount ticket granted ✅");
 } else {
-    console.log("Regular ticket only ❌")
+  console.log("Regular ticket only ❌");
 }
